931e4c171c5fab23de352a9222eabd3956b4de03
mtw-programmer <visnia58@gmail.com>
2024-02-16 17:17:32 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import helmet from 'helmet'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
+import * as cookieParser from 'cookie-parser'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+
+  app.use(cookieParser())
+  app.enableCors({
+    origin: configService.get('TARGET_SITE', 'localhost:3000'),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+  app.use(helmet())
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+  app.setGlobalPrefix('api')
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(configService.get('PORT', 3000))
+}
+bootstrap()
