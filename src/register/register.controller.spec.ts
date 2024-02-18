d302bac0b9ba3364f359f5f14df317b0e81bd7d7
mtw-programmer <visnia58@gmail.com>
2024-02-18 21:50:47 +0100

    test(register): add response test to controller
    and disconnect prisma after all

--- src/register/register.controller.spec.ts
+++ src/register/register.controller.spec.ts
@@ -6,6 +6,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('RegisterController', () => {
   let controller: RegisterController;
+  let registerService: RegisterService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +16,28 @@ describe('RegisterController', () => {
     }).compile();
 
     controller = module.get<RegisterController>(RegisterController);
+    registerService = module.get<RegisterService>(RegisterService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await prismaService.$disconnect();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call registerService.register and return the response', async () => {
+    const registerData = { username: 'test3', password: '1234567890' };
+    const responseMock: any = { send: jest.fn() } as any;
+
+    const registerServiceSpy = jest.spyOn(registerService, 'register').mockResolvedValueOnce({ message: 'You have been successfully registered' });
+
+    await controller.account(registerData, responseMock);
+
+    expect(registerServiceSpy).toHaveBeenCalledWith(registerData);
+
+    expect(responseMock.send).toHaveBeenCalledWith({ message: 'You have been successfully registered' });
   });
 });
