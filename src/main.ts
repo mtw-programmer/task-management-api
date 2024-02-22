c8471d216ed6e28f3503e17f6a7fd16e4159d3b1
mtw-programmer <visnia58@gmail.com>
2024-02-22 21:03:52 +0100

    feat(session): setup with redis

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import RedisStore from 'connect-redis';
+import { createClient } from "redis";
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const redisClient = createClient();
+  redisClient.connect();
+
+  let redisStore = new RedisStore({
+    client: redisClient,
+    prefix: "tasks:",
+  });
 
   app.use(cookieParser());
   app.enableCors({
@@ -31,6 +42,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    session({
+      store: redisStore,
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   await app.listen(configService.get('PORT', 3000));
 }
