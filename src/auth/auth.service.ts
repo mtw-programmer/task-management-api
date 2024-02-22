3e0a0518e4540a65914beb861a446bb6e439a162
mtw-programmer <visnia58@gmail.com>
2024-02-22 19:25:26 +0100

    feat(auth): create simple service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger, BadRequestException } from '@nestjs/common';
+import * as argon2 from 'argon2';
+import { UsersService } from 'src/users/users.service';
+import { Auth } from './auth.validator';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+  ) {}
+
+  private readonly logger = new Logger(AuthService.name);
+
+  async login({ username, password }: Auth):Promise<any> {
+    const user = await this.usersService.findOne(username);
+
+    if (!await argon2.verify(user.password, password)) {
+      this.logger.warn(`${username} failed to login`);
+      throw new BadRequestException('Invalid username or password');
+    }
+
+    this.logger.log(`${username} logged in`);
+    return { message: 'Successfully logged in' };
+  }
+}
