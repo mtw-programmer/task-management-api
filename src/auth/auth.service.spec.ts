6aa3aec60a60fcadbdfe9c2122633962ee0fdb0c
mtw-programmer <visnia58@gmail.com>
2024-02-22 23:45:02 +0100

    feat(auth): write tests for service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
+import { RegisterService } from 'src/register/register.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
+  let registerService: RegisterService;
   let prismaService: PrismaService;
 
   beforeEach(async () => {
@@ -12,11 +16,14 @@ describe('AuthService', () => {
       providers: [
         AuthService,
         UsersService,
+        RegisterService,
         PrismaService
       ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    registerService = module.get<RegisterService>(RegisterService);
     prismaService = module.get<PrismaService>(PrismaService);
   });
 
@@ -25,6 +32,27 @@ describe('AuthService', () => {
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('returns 404 when user was not found', async () => {
+    expect(authService.login({ username: '404', password: '1234567890' })).rejects.toThrow(
+      new NotFoundException()
+    );
+  });
+
+  it('returns 400 when password is invalid', async () => {
+    await registerService.register({ username: 'auth_test1', password: '1234567890' });
+    await expect(authService.login({ username: 'auth_test1', password: 'Invalid_Password' })).rejects.toThrow(
+      new BadRequestException(['Invalid login or password'])
+    );
+    await usersService.deleteOne('auth_test1');
+  });
+
+  it('returns 200 when correct credentials were given', async () => {
+    await registerService.register({ username: 'auth_test1', password: '1234567890' });
+    const res = await authService.login({ username: 'auth_test1', password: '1234567890' });
+    expect(res.message).toBeDefined();
+    await usersService.deleteOne('auth_test1');
   });
 });
