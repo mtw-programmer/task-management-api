b06ef08d451f369627e283dcab1596bc2b9cf788
mtw-programmer <visnia58@gmail.com>
2024-02-28 10:31:09 +0100

    feat(tasks): create getters in service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { task } from '@prisma/client';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor (private prisma: PrismaService) {}
+
+  async findOne(id: number):Promise<task> {
+    return await this.prisma.task
+      .findUniqueOrThrow({
+        where: { id }
+      })
+      .catch(() => {
+        throw new NotFoundException();
+      });
+  }
+
+  async getAll():Promise<task[]> {
+    return await this.prisma.task.findMany();
+  }
+}
