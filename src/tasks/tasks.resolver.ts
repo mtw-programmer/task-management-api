deb0a4d048e191f6013b6333079c8b8d851a64e0
mtw-programmer <visnia58@gmail.com>
2024-03-02 22:53:43 +0100

    fix(tasks): add auth guard to getTasks

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -1,12 +1,15 @@
 import { Resolver, Query, Context } from '@nestjs/graphql';
 import { TasksService } from './tasks.service';
 import { Task } from '@prisma/client';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Resolver('Task')
 export class TasksResolver {
   constructor (private readonly tasksService: TasksService) {}
 
   @Query()
+  @UseGuards(AuthGuard)
   async getTasks(@Context() context: { req: Request }):Promise<Task[]> {
     const { req } = context;
     return await this.tasksService.getAll(req);
