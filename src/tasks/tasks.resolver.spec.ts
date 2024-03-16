19bae494da24613d78d23dbd648eadbdbeb28783
mtw-programmer <visnia58@gmail.com>
2024-03-16 13:45:27 +0100

    feat(tasks): setup tests

--- src/tasks/tasks.resolver.spec.ts
+++ src/tasks/tasks.resolver.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksResolver } from './tasks.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TasksService } from './tasks.service';
+import { UsersService } from 'src/users/users.service';
 
 describe('TasksResolver', () => {
   let resolver: TasksResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksResolver],
+      providers: [
+        TasksResolver,
+        TasksService,
+        UsersService,
+        PrismaService
+      ],
+      controllers: [
+        AuthGuard
+      ]
     }).compile();
 
     resolver = module.get<TasksResolver>(TasksResolver);
+  });
+
+  afterAll(async () => {
+    await prismaService.$disconnect();
   });
 
   it('should be defined', () => {
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterService } from 'src/register/register.service';
+import { UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let tasksService: TasksService;
+  let usersService: UsersService;
+  let registerService: RegisterService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        UsersService,
+        PrismaService,
+        RegisterService
+      ],
+      controllers: [
+        AuthGuard
+      ]
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+    usersService = module.get<UsersService>(UsersService);
+    registerService = module.get<RegisterService>(RegisterService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await prismaService.$disconnect();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(tasksService).toBeDefined();
   });
 });
