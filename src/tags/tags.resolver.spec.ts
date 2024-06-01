6c73a6d3e09a6a55998e0451a35de0235a76dd8e
mtw-programmer <visnia58@gmail.com>
2024-06-02 00:27:08 +0200

    feat(tags): create resolver

--- src/tags/tags.resolver.spec.ts
+++ src/tags/tags.resolver.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { TagsResolver } from './tags.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TagsService } from './tags.service';
+import { UsersService } from 'src/users/users.service';
+import { Tag } from '@prisma/client';
+
+function createContext(data: any): any {
+  const ctx = new ExecutionContextHost([]);
+  ctx.switchToHttp().getRequest = () => data.req;
+  return ctx;
+}
 
 describe('TagsResolver', () => {
   let resolver: TagsResolver;
+  let tagsService: TagsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TagsResolver],
+      providers: [
+        TagsResolver,
+        TagsService,
+        UsersService,
+        PrismaService
+      ],
+      controllers: [
+        AuthGuard
+      ]
     }).compile();
 
     resolver = module.get<TagsResolver>(TagsResolver);
+    tagsService = module.get<TagsService>(TagsService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await prismaService.$disconnect();
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('returns tag with the given id', async () => {
+    const tag: Tag = { id: 1, userId: 1, title: 'Tag 1', color: 'ff0000' };
+
+    jest.spyOn(tagsService, 'findOne').mockResolvedValue(tag);
+
+    const ctx = createContext({ req: { session: { user: 1 } } });
+
+    const res = await resolver.getTag(ctx, 1);
+    expect(res).toEqual(tag);
+  });
+
+  it('returns all tags', async () => {
+    const tags: Tag[] = [
+      { id: 1, userId: 1, title: 'Tag 1', color: 'ff0000' },
+      { id: 2, userId: 1, title: 'Tag 2', color: '0000ff' }
+    ];
+
+    jest.spyOn(tagsService, 'getAll').mockResolvedValue(tags);
+
+    const ctx = createContext({ req: { session: { user: 1 } } });
+
+    const res = await resolver.getTags(ctx);
+    expect(res).toEqual(tags);
   });
 });
--- src/tags/tags.resolver.ts
+++ src/tags/tags.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Context } from '@nestjs/graphql';
+import { TagsService } from './tags.service';
+import { Tag } from '@prisma/client';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@Resolver()
-export class TagsResolver {}
+@Resolver('Tag')
+export class TagsResolver {
+  constructor (private readonly tagsService: TagsService) {}
+
+  @Query()
+  @UseGuards(AuthGuard)
+  async getTag(@Context() context: { req: Request }, id: number):Promise<Tag> {
+    const { req } = context;
+    return await this.tagsService.findOne(req, id);
+  }
+  
+  @Query()
+  @UseGuards(AuthGuard)
+  async getTags(@Context() context: { req: Request }):Promise<Tag[]> {
+    const { req } = context;
+    return await this.tagsService.getAll(req);
+  }
+}
