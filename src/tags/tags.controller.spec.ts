a8563238d0e04f266285391b361192f7a13b677a
mtw-programmer <visnia58@gmail.com>
2024-06-02 01:53:25 +0200

    test(tags): fix controller test

--- src/tags/tags.controller.spec.ts
+++ src/tags/tags.controller.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagsController } from './tags.controller';
+import { TagsService } from './tags.service';
+import { Request, Response } from 'express';
 
 describe('TagsController', () => {
   let controller: TagsController;
+  let tagsService: TagsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TagsController],
+      providers: [
+        {
+          provide: TagsService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({ message: 'Successfully created a new tag' }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TagsController>(TagsController);
+    tagsService = module.get<TagsService>(TagsService);
   });
 
   it('should be defined', () => {
