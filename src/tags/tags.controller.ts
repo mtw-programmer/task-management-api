393e6540d13da1181357c568ea3a058a94c5872d
mtw-programmer <visnia58@gmail.com>
2024-06-02 01:53:57 +0200

    feat(tags): setup controller

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Res, Req } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { TagsService } from './tags.service';
+import { Tag } from './tags.validator';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+
+  @Post('create')
+  async create(
+    @Req() req: Request,
+    @Body(new ValidationPipe()) Tag: Tag,
+    @Res() res: Response,
+  ) {
+    const { message } = await this.tagsService.create(req, Tag);
+    return res.send({ message });
+  }
+}
