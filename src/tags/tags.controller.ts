4acad202c0479a0abd3648992913f208592a33dd
mtw-programmer <visnia58@gmail.com>
2024-06-02 02:16:10 +0200

    docs(tags): create tags/create

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Post, Body, ValidationPipe, Res, Req } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { TagsService } from './tags.service';
 import { Tag } from './tags.validator';
+import { SuccessResponse, BadRequest, InternalServerError } from './tags.docs';
 
 @Controller('tags')
+@ApiTags('Tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create new tag' })
+  @ApiResponse(SuccessResponse)
+  @ApiResponse(BadRequest)
+  @ApiResponse(InternalServerError)
   async create(
     @Req() req: Request,
     @Body(new ValidationPipe()) Tag: Tag,
