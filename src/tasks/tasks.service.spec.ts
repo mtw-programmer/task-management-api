ef356c36894b88f6810dcf12f4857be0077cb245
mtw-programmer <visnia58@gmail.com>
2024-06-05 23:18:26 +0200

    test(tasks): put get tests in describe
    for service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -51,39 +51,41 @@ describe('TasksService', () => {
     await expect(tasksService.getAll(req)).rejects.toThrow(UnauthorizedException);
   });
 
-  it('throws 404 when task id not found', async () => {
-    const req = { session: { user: 1 } };
-    usersService.idExists = jest.fn().mockResolvedValue(true);
-    // No 0 ID in db, so rejects
-    await expect(tasksService.findOne(req, 0)).rejects.toThrow(NotFoundException);
-  });
-
-  it('returns task with the given id for authenticated user', async () => {
-    const req = { session: { user: 1 } };
-    usersService.idExists = jest.fn().mockResolvedValue(true);
-
-    const task: Task = { id: 1, userId: 1, title: 'Task 1', details: 'details', status: 'BACKLOG' };
-
-    const findOneSpy = jest.spyOn(tasksService['prisma'].task, 'findUniqueOrThrow').mockResolvedValue(task);
-    const res = await tasksService.findOne(req, 1);
-
-    expect(res).toEqual(task);
-    expect(findOneSpy).toHaveBeenCalledWith({ where: { id: 1, userId: 1 } });
-  });
-
-  it('returns all tasks for authenticated user', async () => {
-    const req = { session: { user: 1 } };
-    usersService.idExists = jest.fn().mockResolvedValue(true);
-
-    const tasks: Task[] = [
-      { id: 1, userId: 1, title: 'Task 1', details: 'details', status: 'BACKLOG' },
-      { id: 2, userId: 1, title: 'Task 2', details: 'details', status: 'IN_PROGRESS' },
-    ];
-
-    const findOneSpy = jest.spyOn(tasksService['prisma'].task, 'findMany').mockResolvedValue(tasks);
-    const res = await tasksService.getAll(req);
-
-    expect(res).toEqual(tasks);
-    expect(findOneSpy).toHaveBeenCalledWith({ where: { userId: 1 } });
+  describe('Get', () => {
+    it('throws 404 when task id not found', async () => {
+      const req = { session: { user: 1 } };
+      usersService.idExists = jest.fn().mockResolvedValue(true);
+      // No 0 ID in db, so rejects
+      await expect(tasksService.findOne(req, 0)).rejects.toThrow(NotFoundException);
+    });
+  
+    it('returns task with the given id for authenticated user', async () => {
+      const req = { session: { user: 1 } };
+      usersService.idExists = jest.fn().mockResolvedValue(true);
+  
+      const task: Task = { id: 1, userId: 1, title: 'Task 1', details: 'details', status: 'BACKLOG' };
+  
+      const findOneSpy = jest.spyOn(tasksService['prisma'].task, 'findUniqueOrThrow').mockResolvedValue(task);
+      const res = await tasksService.findOne(req, 1);
+  
+      expect(res).toEqual(task);
+      expect(findOneSpy).toHaveBeenCalledWith({ where: { id: 1, userId: 1 } });
+    });
+  
+    it('returns all tasks for authenticated user', async () => {
+      const req = { session: { user: 1 } };
+      usersService.idExists = jest.fn().mockResolvedValue(true);
+  
+      const tasks: Task[] = [
+        { id: 1, userId: 1, title: 'Task 1', details: 'details', status: 'BACKLOG' },
+        { id: 2, userId: 1, title: 'Task 2', details: 'details', status: 'IN_PROGRESS' },
+      ];
+  
+      const findOneSpy = jest.spyOn(tasksService['prisma'].task, 'findMany').mockResolvedValue(tasks);
+      const res = await tasksService.getAll(req);
+  
+      expect(res).toEqual(tasks);
+      expect(findOneSpy).toHaveBeenCalledWith({ where: { userId: 1 } });
+    });
   });
 });
