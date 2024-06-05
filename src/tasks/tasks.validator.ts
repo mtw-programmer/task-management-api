db06dc4fd3f8acbfb1626649bec5383adc3efe8b
mtw-programmer <visnia58@gmail.com>
2024-06-05 23:14:34 +0200

    feat(tasks): create basic validator

---
+++ src/tasks/tasks.validator.ts
@@ -0,0 +1,22 @@
+import { IsArray, IsNumber, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Tag {
+  @ApiProperty({ minLength: 1, maxLength: 50 })
+  @IsString()
+  @Length(1, 50)
+  title: string;
+
+  @ApiProperty({ minLength: 1, maxLength: 2000 })
+  @IsString()
+  @Length(1, 2000)
+  details?: string;
+  
+  @ApiProperty({
+    type: [Number],
+    description: 'Array of tag IDs'
+  })
+  @IsArray()
+  @IsNumber({}, { each: true, message: 'All elements must be tag IDs' })
+  tags?: number[];
+}
